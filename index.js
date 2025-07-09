08b7541e4a5a2c8eca296572b0685ce59f4fc983
PaytonORogers <payton.o.rogers@gmail.com>
2025-07-09 11:26:00 -0700

    fixed li and button js

--- index.js
+++ index.js
@@ -11,21 +11,32 @@ var doneList = document.querySelector('.done');
 //function for button
 let test = (input) => {
     let newLineItem = document.createElement('li');
+    newLineItem.innerHTML = input
     let checkBox = document.createElement('input');
-    checkBox.type = 'checkbox'
+        checkBox.type = 'checkbox'
     let deleteButton = document.createElement('button');
     deleteButton.innerText = "Delete";
-    deleteButton.addEventListener('click', toDoList.removeChild(newLineItem));
+    deleteButton.addEventListener('click', () => {return doneList.contains(newLineItem) ? doneList.removeChild(newLineItem) : toDoList.removeChild(newLineItem)});
     checkBox.addEventListener('click', () => {
-        toDoList.removeChild(newLineItem);
+        // toDoList.removeChild(newLineItem);
         doneList.appendChild(newLineItem);
     });
     newLineItem.appendChild(checkBox);
-    newLineItem.appendChild(input);
+    // newLineItem.appendChild(input);
     newLineItem.appendChild(deleteButton);
     toDoList.appendChild(newLineItem);
 }
 
 
 //button added to event
-addToListButton.addEventListener('click', () => {test(userInput.value)})
+addToListButton.addEventListener('click', () => test(userInput['value']))
+
+//line 25 causes it to break
+
+//line 19 - 23 break
+//line 20 -23 break and 25
+
+//added line 14 amended 19 and 32
+// delete does not work if box is checked
+
+//line 19 return doneList.removeChild(newLineItem
